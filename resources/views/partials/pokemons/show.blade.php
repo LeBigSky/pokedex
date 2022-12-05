6aeb2a99628d0d5c679afc3c35d25c137e342971
Hackashi <nexagray@gmail.com>
2022-12-05 16:58:54 +0100

    design img card

--- resources/views/partials/pokemons/show.blade.php
+++ resources/views/partials/pokemons/show.blade.php
@@ -2,19 +2,19 @@
  <div class="container">
   <a href="{{ route('home') }}" class="btn btn-info text-white my-5">Retour</a>
  </div>
-    <div class="container d-flex justify-content-center mt-5">
+    <div class="container d-flex justify-content-center mt-2">
         <div class="card" style="background: linear-gradient(180deg, rgb(228, 174, 48) 23%, rgba(249, 229, 3, 0.584) 95%);">
           <div class="border rounded m-3">
-            <img src="{{ asset('storage/'.$monster->image->src) }}"  class="card-img-top" alt="" style="width:350px;"/>
+            <img src="{{ asset('storage/'.$monster->image->src) }}"  class="card-img-top" alt="" style="width:310px; height:300px"/>
           </div>
-            <div class="card-body style" >
+            <div class="card-body" >
               <h2 class="card-title text-center border rounded" style="font-weight: bold; background-color: white" > {{ $monster->nom }}</h2>
-              <div class="border rounded p-2 my-1" style="background-color: white">
+              <div class="border rounded p-2" style="background-color: white">
                 <h3 class="card-text text-center">type: {{ $monster->type->type }}</h3>
               <h3 class="card-text text-center">level: {{ $monster->level }}</h3>
               </div>
               
-              <div class="d-flex justify-content-around my-2 gap-2">
+              <div class="d-flex justify-content-between my-3 gap-2">
                 <a href="{{ $monster->id }}/edit" class="btn btn-info w-50 ">Edit</a>
                 <form action="/monster/{{ $monster->id }}" method="POST">
                   @csrf
