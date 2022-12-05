aef33641f055b267fcd43e488cd1c3e4d6f29a6e
Hackashi <nexagray@gmail.com>
2022-12-05 15:48:55 +0100

    add navbar with design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
+    @include('partials.nav')
     @yield('content')
 </body>
 </html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,19 @@
+<section>
+    <nav class="navbar nav">
+        <div class="container-fluid">
+            <div id="title" class="d-flex gap-2">
+    <h1  style="color:yellow">Pocket</h1>
+               <img src="{{ asset('assets/img/a94f4d75a2e429a20838d28d2ae2b996.png') }}" width="50px;" alt="">
+            <h1 style="color:yellow">Monsters</h1> 
+            </div>
+           
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            </ul>
+          </div>
+        </div>
+      </nav>
+</section>
